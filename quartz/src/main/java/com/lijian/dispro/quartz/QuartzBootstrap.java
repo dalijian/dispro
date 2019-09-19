58a82a73ab58c2f427ea6a02c930f6c76570b072
lijian <237922011@qq.com>
2019-09-19 23:48:24 +0800

    commit change cron express,solve wait_timeout

--- quartz/src/main/java/com/lijian/dispro/quartz/QuartzBootstrap.java
+++ quartz/src/main/java/com/lijian/dispro/quartz/QuartzBootstrap.java
@@ -16,11 +16,9 @@ public class QuartzBootstrap {
     //    自定义 任务启动
     @PostConstruct
     public void start() {
-
-
         // 支持 动态 修改 cron 表达式，但是 jod 与 trigger  必须是 1对1
         try {
-            myScheduler.startJob("0 0 0 1/1 * ?", "job_crawl_weather", "job_crawl", "定时抓取天气（一天一次）", WeatherCrawl.class);
+            myScheduler.startJob("0 0 5 * * ? *", "job_crawl_weather", "job_crawl", "定时抓取天气（一天一次）", WeatherCrawl.class);
         } catch (SchedulerException e) {
             e.printStackTrace();
         }
